772fd8dd851cdfbbaa7d1134493f847c471b988f
martin <martin.adam.stoffel@gmail.com>
2019-07-15 10:03:03 +0100

    roh and fitness data

--- LRT_ROH_data.R
+++ LRT_ROH_data.R
@@ -10,6 +10,8 @@ library("MasterBayes")
 library(pedigreemm)
 library(brms)
 library(MCMCglmm)
+library(rlang)
+
 #### database connection ######
 
 dbname <- "../sheep/data/db/StKilda_Data.accdb"
@@ -36,6 +38,58 @@ sheep_ped <- read_delim("../sheep/data/SNP_chip/20190208_Full_Pedigree.txt", del
         orderPed() %>%
         rename(Mother = MOTHER, Father = FATHER)
 #save(sheep_ped,  file = "model_in/sheep_ped.RData")
+
+# annual fitness
+annual_fitness <- read_delim("data/1_Annual_Fitness_Measures_April_20190501.txt", delim = "\t")
+annual_fitness %<>% 
+  dplyr::select(-contains("EASTING")) %>% 
+  dplyr::select(-contains("NORTHING")) %>% 
+  dplyr::select(-contains("Cartesian"))
+
+##### ROH data #####
+file_path <- "output/ROH/roh_nofilt.hom"
+file <- "roh_nofilt"
+roh_lengths <- fread(file_path)
+
+
+# roh_crit = c("short", "medium", "long", "all")
+calc_froh_classes <- function(roh_crit, roh_lengths) {
+  
+  roh_filt <- dplyr::case_when(
+    roh_crit == "short"  ~ expr(KB < 3130),
+    roh_crit == "medium" ~ expr((KB > 3130)&(KB < 12500)),
+    roh_crit == "long"   ~ expr(KB > 12500),
+    roh_crit == "all" ~ expr(KB > 0)
+  )
+  
+  roh_lengths %>%
+    dplyr::group_by(FID) %>%
+    #filter({{ roh_filt }}) %>% 
+    filter(!!roh_filt) %>% 
+    dplyr::summarise(KBSUM = sum(KB)) %>% 
+    mutate(FROH = KBSUM / 2534344) %>% 
+    dplyr::select(FID, FROH) %>% 
+    rename(ID = FID, !! paste0("FROH_", roh_crit) := FROH)
+  
+}
+
+# proportion of ROH length classes in each genome. Individuals which
+# do not have long ROH have 0 for this class.
+froh <- purrr::map(c("short", "medium", "long", "all"), calc_froh_classes,  roh_lengths) %>% 
+          purrr::reduce(left_join, by = "ID") %>% 
+          replace_na(list(FROH_long = 0))
+
+
+# dataset for modeling
+fitness_data <- annual_fitness %>% 
+  dplyr::select(ID, OffspringBorn, OffspringSurvived, SheepYear, Age, Survival,
+         BIRTHYEAR, SEX, MOTHER, BIRTHWT, CapMonth, Weight, Hindleg, ID.CapYear) %>% 
+  left_join(froh, by = "ID")
+
+save(fitness_data, file = "model_in/fitness_roh_df.RData")
+save(sheep_ped, file = "model_in/sheep_ped.RData")
+
+
 
 # # females: offspring
 # sheep_ped %>% 
@@ -72,74 +126,20 @@ sheep_ped <- read_delim("../sheep/data/SNP_chip/20190208_Full_Pedigree.txt", del
 #                       BirthYear, MumID) %>% 
 #         filter(Status %in% c("Dead", "EstDead")) %>% 
 #         mutate_at(c("ID", "MumID", "BirthYear", "DeathYear", "Sex"), as.character)
-
-
-# annual fitness
-annual_fitness <- read_delim("data/1_Annual_Fitness_Measures_April_20190501.txt", delim = "\t")
-annual_fitness %<>% 
-  dplyr::select(-contains("EASTING")) %>% 
-  dplyr::select(-contains("NORTHING")) %>% 
-  dplyr::select(-contains("Cartesian"))
-
-##### ROH data #####
-file_path <- "output/ROH/roh_nofilt.hom"
-file <- "roh_nofilt"
-roh_lengths <- fread(file_path)
-
-library(rlang)
-
-# roh_crit = c("short", "medium", "long", "all")
-calc_froh_classes <- function(roh_crit, roh_lengths) {
-  
-  roh_filt <- dplyr::case_when(
-    roh_crit == "short"  ~ expr(KB < 3130),
-    roh_crit == "medium" ~ expr((KB > 3130)&(KB < 12500)),
-    roh_crit == "long"   ~ expr(KB > 12500),
-    roh_crit == "all" ~ expr(KB > 0)
-  )
-  
-  roh_lengths %>%
-    dplyr::group_by(FID) %>%
-    #filter({{ roh_filt }}) %>% 
-    filter(!!roh_filt) %>% 
-    dplyr::summarise(KBSUM = sum(KB)) %>% 
-    mutate(FROH = KBSUM / 2534344) %>% 
-    dplyr::select(FID, FROH) %>% 
-    rename(ID = FID, !! paste0("FROH_", roh_crit) := FROH)
-  
-}
-
-froh <- purrr::map(c("short", "medium", "long", "all"), calc_froh_classes,  roh_lengths) %>% 
-          purrr::reduce(left_join, by = "ID")
-
-froh <- roh_lengths %>%
-        dplyr::group_by(FID) %>%
-        #filter((KB < 3130)) %>% 
-        #filter((KB > 3130)&(KB < 12500)) %>% 
-        #filter((KB > 12500)) %>% 
-        #filter(KB < 3130) %>% 
-        dplyr::summarise(KBAVG = mean(KB), KBSUM = sum(KB)) %>%
-        mutate(MBSUM = KBSUM / 1000) %>%
-        mutate(FROH = KBSUM / 2534344) %>%
-        rename(ID = FID) %>% 
-        mutate(ID = as.character(ID))
-
-# dataset for modeling
-
 # individuals with both lrt and roh info
-lrt_roh_df <- LRT %>% 
-               # mutate(ID = as.character(ID)) %>% 
-                inner_join(froh, by = "ID") %>% 
-                filter(!is.na(Sex))
-
-# check distribution
-lrt_roh_df %>% 
-        filter(!is.na(Sex)) %>% 
-        mutate(Sex = as.factor(Sex)) %>% 
-        ggplot(aes(n_offspring)) + 
-                geom_histogram(bins = 100) +
-                scale_y_sqrt() + 
-                facet_wrap(Sex~., scales = "free_x")
-
-save(lrt_roh_df, "model_in/lrt_roh_df.RData")
-save(sheep_ped, "model_in/sheep_ped.RData")
+# lrt_roh_df <- LRT %>% 
+#   # mutate(ID = as.character(ID)) %>% 
+#   inner_join(froh, by = "ID") %>% 
+#   filter(!is.na(Sex))
+# 
+# # check distribution
+# lrt_roh_df %>% 
+#   filter(!is.na(Sex)) %>% 
+#   mutate(Sex = as.factor(Sex)) %>% 
+#   ggplot(aes(n_offspring)) + 
+#   geom_histogram(bins = 100) +
+#   scale_y_sqrt() + 
+#   facet_wrap(Sex~., scales = "free_x")
+# 
+# save(lrt_roh_df, "model_in/lrt_roh_df.RData")
+# save(sheep_ped, "model_in/sheep_ped.RData")
